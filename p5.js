14164b7a6556f5fe7c64e3cc77411b9dc740a5e4
Mukeshwar Mandal <mandalmandip333@gmail.com>
2023-12-28 20:43:20 +0545

    Practise set 5 is done

---
+++ p5.js
@@ -0,0 +1,24 @@
+//question 1
+
+let arr =[1,3,4,10];
+// let num = prompt("enter any number ");
+let num = 78;
+num= Number.parseInt(num);
+arr.push(num);
+console.log(arr);
+
+// question 2
+// requires prompt
+
+//question 3
+array = arr.map((a)=>a*a);
+console.log(array);
+
+//question 4
+arr1=arr.filter((a)=>a%10==0);
+console.log(arr1);
+
+// Question 5 
+arr2 = [1,2,3,4,5];
+arr9=arr2.reduce((a,b)=>a*b);
+console.log(arr9);
