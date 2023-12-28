64632dd0db1e6c03f25095e93f766cad2d71f413
Mukeshwar Mandal <mandalmandip333@gmail.com>
2023-12-28 20:31:35 +0545

    added new array methods

--- arrays&loop.js
+++ arrays&loop.js
@@ -3,4 +3,13 @@ let newArr=arr.map((value)=> {
    return (value*2);
 })
 console.log(newArr);
+let arr2=arr.filter((a)=>{
+   return a%2==0;
 
+});
+console.log(arr2);
+ let arr3=arr.reduce((a,b)=>{
+   return a-b;
+
+ });
+ console.log(arr3);
