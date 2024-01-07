7a699b80ec7a1ebc6e0a72652915a2f50360d46f
Mukeshwar Mandal <mandalmandip333@gmail.com>
2024-01-07 19:14:39 +0545

    Practised some table navigations

--- tablenav.js
+++ tablenav.js
@@ -1,1 +1,8 @@
-console.log(document.body.firstChild)
+// let a =document.body.firstElementChild.firstElementChild;
+let a =document.body.firstElementChild;
+
+console.log(a.rows);
+console.log(a);
+console.log(a.tHead);
+console.log(a.caption);
+console.log()
